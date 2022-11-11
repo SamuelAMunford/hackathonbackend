c49c8f94bc80b9b76e685a940e2338ef847be1fd
Matthew Rodriguez (TATA CONSULTANCY SERVICES LTD) <v-matthewro@microsoft.com>
2022-11-11 14:16:20 -0800

    adsd

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+const PORT = 5000;
+
+app.use(express.json({limit:"30mb",extended: true})); 
+app.use(express.urlencoded({limit:"30mb",extended: true}));
+app.use(cors());
+
+const visitors = []
+app.use('/',(req,res) => {
+    res.send('BHF API')
+    console.log(req.body)
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+// app.listen(PORT)
+module.exports = app
