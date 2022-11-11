681d9686a6b35b709f38122deefe0cdcfe438b34
Matthew Rodriguez (TATA CONSULTANCY SERVICES LTD) <v-matthewro@microsoft.com>
2022-11-11 14:35:50 -0800

    backend

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express();
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json({limit:"30mb",extended: true})); 
 app.use(express.urlencoded({limit:"30mb",extended: true}));
-app.use(cors());
 
 const visitors = []
 app.use('/',(req,res) => {
-    res.send('hackathon API')
+    res.send('visitor API')
     console.log(req.body)
 })
 
